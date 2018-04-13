71808296303ac2bf0a21046b237344195bf91ce7
dongqn <36226384+dongqn@users.noreply.github.com>
2018-04-13 23:14:38 +0700

    Update hxx.sh

--- hxx.sh
+++ hxx.sh
@@ -9,11 +9,15 @@ output "Upgrade OS"
 sudo apt-get update
 output "Upgrade OS - Done!"
 output "Install CPUminer"
-sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git screen
-cd /home/dongqn/
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+cd /root
 git clone https://github.com/JayDDee/cpuminer-opt
 cd cpuminer-opt
+chmod +x *
 ./build.sh
-cp cpuminer ../
+mv cpuminer dongqn
+make
+cp dongqn ../
 output "";
-cd
+cd /root
